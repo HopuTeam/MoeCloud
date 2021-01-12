00fb7fcc496440fd7030e0e22adef64a1063d46e
Skilemon <35643936+Skilemon@users.noreply.github.com>
2021-01-12 10:30:04 +0800

    Fixed

--- MoeCloud.Server/MoeCloud.Api/Startup.cs
+++ MoeCloud.Server/MoeCloud.Api/Startup.cs
@@ -43,6 +43,7 @@ namespace MoeCloud.Api
             {
                 //options.Filters.Add<ErrorFilter>();
             });
+            services.AddScoped<ILogic.ICommon, Logic.CommonLogic>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ MoeCloud.Server/MoeCloud.Logic/UserLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoeCloud.Logic
+{
+    public class UserLogic : ILogic.IUser
+    {
+        private Data.CoreEntities EF { get; }
+        public UserLogic(Data.CoreEntities _ef)
+        {
+            EF = _ef;
+        }
+
+
+    }
+}
